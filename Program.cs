b80739c2791d32e73aca3aab4e2843f18a717145
sapatelbaps <sapatelbaps@gmail.com>
2019-03-07 15:13:46 -0800

    "donet new console" command used to create a new asp net core console application.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASPNETCoreMVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
