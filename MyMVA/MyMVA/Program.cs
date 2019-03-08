f17f632571c1a271531619b01edcc3b078d399f5
sapatelbaps <sapatelbaps@gmail.com>
2019-03-08 01:22:58 -0800

    .

--- MyMVA/MyMVA/Program.cs
+++ MyMVA/MyMVA/Program.cs
@@ -8,6 +8,7 @@ namespace MyMVA
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
