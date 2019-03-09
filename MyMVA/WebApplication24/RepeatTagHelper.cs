a7837e8a864b5b4be81765b6e09dd9ed85fee58c
sapatelbaps <sapatelbaps@gmail.com>
2019-03-08 21:03:18 -0800

    RepeatTagHelper added - custom tag in cshtml view.

---
+++ MyMVA/WebApplication24/RepeatTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication24
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    public class RepeatTagHelper : TagHelper
+    {
+        public int Count { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                output.Content.AppendHtml(await output.GetChildContentAsync(useCachedResult:false));
+            }
+        }
+
+    }
+}
