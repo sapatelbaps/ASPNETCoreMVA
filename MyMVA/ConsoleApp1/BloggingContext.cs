4606b9f75cbfe79d49c2c4c22fc60f296a99ad75
sapatelbaps <sapatelbaps@gmail.com>
2019-07-26 15:21:51 -0700

    Added comments and README.md file at some places as required.

--- MyMVA/ConsoleApp1/BloggingContext.cs
+++ MyMVA/ConsoleApp1/BloggingContext.cs
@@ -16,24 +16,24 @@ namespace ConsoleApp1
         public DbSet<Post> Posts { get; set; }
 
         public string _tenantId { get; private set; }
-        
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyMVA-ConsoleApp1;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=MyMVA-ConsoleApp1;Trusted_Connection=True;MultipleActiveResultSets=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Theme>().HasData(
-            new Theme { ThemeId = 1, Name="MSDN",TitleColor = Color.AliceBlue.Name},
-            new Theme { ThemeId = 2, Name="TechNet", TitleColor = Color.DarkBlue.Name},
-            new Theme { ThemeId = 3, Name="Personal", TitleColor = Color.LightBlue.Name });
+            new Theme { ThemeId = 1, Name = "MSDN", TitleColor = Color.AliceBlue.Name },
+            new Theme { ThemeId = 2, Name = "TechNet", TitleColor = Color.DarkBlue.Name },
+            new Theme { ThemeId = 3, Name = "Personal", TitleColor = Color.LightBlue.Name });
 
             // Way of telling relationship how we want/have configured.
             modelBuilder.Entity<Blog>()
                 .HasMany(b => b.Posts)
                 .WithOne()
-                .HasForeignKey(b=>b.BlogId);
+                .HasForeignKey(b => b.BlogId);
 
             //Configure entity filters
             modelBuilder.Entity<Post>().HasQueryFilter(p => !p.IsDeleted);
@@ -46,13 +46,13 @@ namespace ConsoleApp1
             ChangeTracker.DetectChanges();
 
             foreach (var item in ChangeTracker.Entries().Where(
-                e=>e.State == EntityState.Added && e.Metadata.GetProperties().Any(p=>p.Name=="TenantId")))
+                e => e.State == EntityState.Added && e.Metadata.GetProperties().Any(p => p.Name == "TenantId")))
             {
                 item.CurrentValues["TenantId"] = _tenantId;
             }
 
             foreach (var item in ChangeTracker.Entries<Post>()
-                .Where(e=>e.State == EntityState.Deleted))
+                .Where(e => e.State == EntityState.Deleted))
             {
                 item.State = EntityState.Modified;
                 item.CurrentValues["IsDeleted"] = true;
---
+++ MyMVA/README.md
@@ -0,0 +1,29 @@
+
+# Projects Description
+* **ConsoleApp1**
+>> "dotnet new console" command to create a console applicaiton project  
+>> EFCore Migration in the code using EF Core.
+>> Developed models and setup database every time we run the program and display the data on console.
+* **MyWebApp**
+>> Added default .net core 2.2 website with no authentication to practice Azure deployment 
+>> directly from VS 2019. 
+* **MyMVA**
+>>Simple ASP .NET Core web application with default template.  
+>>Try to read machine property and some property from the "appsettings.json" file.  
+>>WebApp using ASP .NET Core
+>> Read message from appsettings.development.json file in the razor page.  
+>> Use "Configuration Settings" inside index.cshtml
+* **MVCWebApp**
+>> MVC Web Application using traditional .NET Framework 4.7
+>> Added just to view the components and directory structures of various 
+>> files and compare with new .NET Core template files and see how
+>> much its compressed.
+* **WebApplication2**
+>> .NET Core Web API project with default "ValuesController".  
+>> Ticket controller also added with basic operations implementation and 
+>> "Swaggers" API. (Only three lines of code to add 'Swaggers' in the API and
+>> automatically Swaggers will create UI for us to access the Web API operations.) 
+* **WebApplication24** 
+>>RepeatTagHelper class in .NET Core web application.  
+>>Design custom tag to be used in "index.cshtml" and implement its code behind in the .cs file.  
+>>Trying to add QRCode package for two factor authentication. Still in progress..
