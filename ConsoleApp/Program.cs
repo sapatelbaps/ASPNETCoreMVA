a898a03f6d8f654191c77a164d944b311dc5a66e
Sandip <5420792+sapatelbaps@users.noreply.github.com>
2020-06-17 18:00:49 -0700

    Code added to list files and folders for a list of drives/UNC path

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace ASPNETCoreMVA
 {
@@ -7,6 +11,27 @@ namespace ASPNETCoreMVA
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            PrepareTorrentsList();
+        }
+
+        private static void PrepareTorrentsList()
+        {
+            List<string> locations = File.ReadAllLines(@"D:\Git\sapatelbaps\ASPNETCoreMVA\ConsoleApp\Locations.txt").ToList();
+            string outputFile = @"D:\Git\sapatelbaps\ASPNETCoreMVA\ConsoleApp\AllTorrents.txt";
+            StringBuilder content = new StringBuilder();
+
+            foreach (string path in locations)
+            {
+                List<string> items = new List<string>();
+                items.AddRange(Directory.GetDirectories(path).Select(f => path + "~" + Path.GetFileName(f)).ToList());
+                items.AddRange(Directory.GetFiles(path).Select(f => path + "~" + Path.GetFileName(f)).ToList());
+
+                content.AppendLine(path);
+                items.ForEach(i => content.AppendLine(i));
+            }
+
+            File.WriteAllText(outputFile, content.ToString());
         }
     }
 }
--- MyMVA/WebApplication2/Controllers/TicketController.cs
+++ MyMVA/WebApplication2/Controllers/TicketController.cs
@@ -25,7 +25,7 @@ namespace WebApplication2.Controllers
             }
         }
 
-	[HttpGet]
+        [HttpGet]
         public IActionResult Test()
         {
             return new ObjectResult("SwamiShriji");
